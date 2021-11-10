eca8134d93851047812125caee98a066a0646131
takundachirema <takunda.chirema@alumni.uct.ac.za>
2021-11-10 12:59:04 +0200

    use heroku servers for axios

--- src/components/Axios/axios.js
+++ src/components/Axios/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Next we make an 'instance' of it
 const instance = axios.create({
 // .. where we make our configurations
-    baseURL: 'http://algorand-dashboard-backend.herokuapp.com'
+    baseURL: 'https://algorand-dashboard-backend.herokuapp.com'
 });
 
 export default instance;
